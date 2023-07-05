45a2297b2b5f6f0efc165e36f641a5530a203b8b
priyanshuup <109269025+priyanshuup@users.noreply.github.com>
2023-07-05 22:05:38 +0530

    re

---
+++ js/gsap.js
@@ -0,0 +1,53 @@
+gsap.from("#home>.homediv", {
+    opacity: 0,
+    duration: 1,
+    x: -120,
+    ease: "power1.out",
+  });
+  gsap.from("#home>.hometextdiv", {
+    opacity: 0,
+    duration: 1,
+    x: 100,
+    ease: "power1.out",
+  });
+  
+  gsap.from("#filmsubmission>.hometextdiv",
+    {
+      y: 100,
+      opacity: 0,
+      duration: 1,
+      ease: "Expo.easeInOut",
+    },
+    "-=1"
+  );
+  
+  gsap.from(".youtubediv",
+    {
+      x: 100,
+  
+      // delay:.5,
+      opacity: 0,
+      duration: 1,
+  
+      ease: Expo.easeInOut,
+    },
+    "-=1"
+  );
+  
+  gsap.from("#facilities", {
+    scrollTrigger: {
+       start:"top 20%",
+        // end:"top 80%",
+      //   pin: true,
+           markers:true,
+      scroller: "#main",
+      trigger: "#filmsubmission",
+    },
+    y: 20,
+    opacity: 0,
+  //   delay:5,
+    duration: 1.2,
+    ease: Expo.easeInOut,
+    skewY: 3,
+  
+  });
---
+++ js/loco.js
@@ -0,0 +1,43 @@
+function animation() {
+  gsap.registerPlugin(ScrollTrigger);
+
+  // Using Locomotive Scroll from Locomotive https://github.com/locomotivemtl/locomotive-scroll
+
+  const locoScroll = new LocomotiveScroll({
+    el: document.querySelector("#main"),
+    smooth: true,
+  });
+  // each time Locomotive Scroll updates, tell ScrollTrigger to update too (sync positioning)
+  locoScroll.on("scroll", ScrollTrigger.update);
+
+  // tell ScrollTrigger to use these proxy methods for the ".smooth-scroll" element since Locomotive Scroll is hijacking things
+  ScrollTrigger.scrollerProxy("#main", {
+    scrollTop(value) {
+      return arguments.length
+        ? locoScroll.scrollTo(value, 0, 0)
+        : locoScroll.scroll.instance.scroll.y;
+    }, // we don't have to define a scrollLeft because we're only scrolling vertically.
+    getBoundingClientRect() {
+      return {
+        top: 0,
+        left: 0,
+        width: window.innerWidth,
+        height: window.innerHeight,
+      };
+    },
+
+    pinType: document.querySelector("#main").style.transform
+      ? "transform"
+      : "fixed",
+  });
+
+  // each time the window updates, we should refresh ScrollTrigger and then update LocomotiveScroll.
+  ScrollTrigger.addEventListener("refresh", () => locoScroll.update());
+
+  // after everything is set up, refresh() ScrollTrigger and update LocomotiveScroll because padding may have been added for pinning, etc.
+  ScrollTrigger.refresh();
+}
+animation();
+
+
+
